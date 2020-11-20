2d84fc46201d27a86252279c043d3e37fc19888f
vikram singh <58397405+Sparta-9@users.noreply.github.com>
2020-11-20 22:28:49 +0530

    Update mobile prediction.py

--- mobile prediction.py
+++ mobile prediction.py
@@ -1,7 +1,7 @@
 import pandas
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
-data = pandas.read_csv('iphone_price.csv')
+data = pandas.read_csv('Price.csv')
 plt.scatter(data['version'], data['price'])
 plt.show()
 model = LinearRegression()
